d95490daf28c1d8f24299ddec43f3ccdcb6368e7
Your Name <a.krutick@gmail.com>
2025-04-15 20:08:47 +0200

    back button second deply test

--- src/components/Companies/CompanyDetails.jsx
+++ src/components/Companies/CompanyDetails.jsx
@@ -5,7 +5,7 @@ import Skeleton from '@mui/material/Skeleton';
 import LoadingSpinner from '@mui/material/Skeleton';
 
 function CompanyDetails() {
-  tg.BackButton.show();
+  window.Telegram?.WebApp.BackButton.show();
     const navigate = useNavigate();
   const { id } = useParams();
   const { state } = useLocation();
